857645d904874a3b45da30517ea23a5f5020f5f6
Ezepue <ebukaezepue@gmail.com>
2024-03-11 13:47:19 +0100

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,11 +4,11 @@ function factorial (a) {
 	  return 1;
 	}
 	return (a * factorial(a - 1));
-  }
-  const args = process.argv;
-  const num = parseInt(args[2]);
-  if (isNaN(num)) {
+}
+const args = process.argv;
+const num = parseInt(args[2]);
+if (isNaN(num)) {
 	console.log(1);
-  } else {
+} else {
 	console.log(factorial(num));
-  }
+}
