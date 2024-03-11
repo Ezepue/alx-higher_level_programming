e5e23e224dd0aad114a88cbb3729e36b6cab8a68
Ezepue <ebukaezepue@gmail.com>
2024-03-11 14:18:11 +0100

    All Fixed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
-function factorial (n) {
-	return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+function factorial (a) {
+  if (a === 0) {
+    return 1;
+  }
+  return (a * factorial(a - 1));
 }
-  
-console.log(factorial(Number(process.argv[2])));
+const args = process.argv;
+const num = parseInt(args[2]);
+if (isNaN(num)) {
+  console.log(1);
+} else {
+  console.log(factorial(num));
+}
