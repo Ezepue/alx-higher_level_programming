aeb7cb0fc74496fdbdfd51462e5cff2646c7c511
Ezepue <ebukaezepue@gmail.com>
2024-03-11 13:42:56 +0100

    fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 function add (a, b) {
 	console.log(a + b);
-  }
+}
   const args = process.argv;
   const first = parseInt(args[2]);
   const second = parseInt(args[3]);
   if (isNaN(first) || isNaN(second)) {
 	console.log(NaN);
-  } else {
+} else {
 	add(first, second);
-  }
+}
